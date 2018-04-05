fba86e4416ed0b2a43a22b7cf249964e13e5a3de
Thomas Prince <gurneyman@users.noreply.github.com>
2018-04-04 23:14:19 -0400

    Update title

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -7,7 +7,7 @@ import './all.sass'
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="Home | Gatsby + Netlify CMS" />
+    <Helmet title="Home | Tom Prince Development Notes" />
     <Navbar />
     <div>{children()}</div>
   </div>
